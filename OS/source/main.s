afab1cef664bc934acb4b8e77d103fa5c8caf5a3
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2013-08-20 12:41:45 +0200

    Fixed wrong comment character
    I used ; instead of #. Fixed.

--- OS/source/main.s
+++ OS/source/main.s
@@ -1,24 +1,24 @@
 .section .init
 .globl _start
 _start:
-;Load the address for GPIO
+#Load the address for GPIO
 ldr r0,=0x20200000
 
-;Enable the OK LED pin
+#Enable the OK LED pin
 mov r1,#1
 lsl r1,#18
 str r1,[r0,#4]
 
-;Load a string with 1 at LED pin
+#Load a string with 1 at LED pin
 mov r1,#1
 lsl r1,#16
 
 loop$:
 
-;Turn the LED on!
+#Turn the LED on!
 str r1,[r0,#40]
 
-;Wait by counting down
+#Wait by counting down
 mov r2,#0x3F0001
 wait1$:
 sub r2, #1
@@ -26,10 +26,10 @@ cmp r2, #0
 bne wait1$
 
 
-;Turn the LED off
+#Turn the LED off
 str r1,[r0,#28]
 
-;Wait again
+#Wait again
 mov r2,#0x3F0001
 wait2$:
 sub r2, #1
