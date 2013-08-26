d025673a1928a87280ab712106e543f1a3579701
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2013-08-27 00:05:33 +0200

    Added a function to set a Gpio pin

--- OS/source/gpio.s
+++ OS/source/gpio.s
@@ -1,8 +1,12 @@
+
+############# Get Gpio Address #############
 .globl GetGpioAddress
 GetGpioAddress:
 ldr r0,=0x20200000
 mov pc, lr
 
+
+############# Set Gpio Function#############
 .globl SetGpioFunction
 SetGpioFunction:
 #Check the input
@@ -25,5 +29,51 @@ bhi functionLoop$
 add r2, r2, lsl #1
 lsl r1, r2
 str r1,[r0]
+pop {pc}
+
+
+############# Set Gpio #############
+.globl SetGpio
+SetGpio:
+pinNum .req r0
+pinVal .req r1
+
+#Return if pinNum < 53
+cmp pinNum,#53
+movhi pc,lr
+
+#Get the gpio address
+push {lr}
+mov r2,pinNum
+.unreq pinNum
+pinNum .req r2
+bl GetGpioAddress
+gpioAddr .req r0
+
+# Add 4 to the gpioAddr if
+# pinNum > 32 (We're in the 
+# second set of banks)
+pinBank .req r3
+lsr pinBank,pinNum,#5
+lsl pinBank,#2
+add gpioAddr,pinBank
+.unreq pinBank
+
+#Calculate correct setBit
+and pinNum,#31
+setBit .req r3
+mov setBit,#1
+lsl setBit,pinNum
+.unreq pinNum
+
+#Set the pin to pinVal
+teq pinVal,#0
+.unreq pinVal
+#Turn pin off
+streq setBit,[gpioAddr,#40]
+#Turn pin on
+strne setBit,[gpioAddr,#28]
+.unreq setBit
+.unreq gpioAddr
 pop {pc}
 
