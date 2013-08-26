aba5b2dc7973152df04ca0c9bf2b277225ec457d
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2013-08-26 10:54:46 +0200

    Made more functions!
    Function to get the gpio address, and a function to set a gpio pin

--- OS/source/gpio.s
+++ OS/source/gpio.s
@@ -3,3 +3,27 @@ GetGpioAddress:
 ldr r0,=0x20200000
 mov pc, lr
 
+.globl SetGpioFunction
+SetGpioFunction:
+#Check the input
+cmp r0, #53
+cmpls r1, #7
+movhi pc,lr
+
+#Call GetGpioAddress
+push {lr}
+mov r2,r0
+bl GetGpioAddress
+
+#Figure out which block of 10 we're in
+functionLoop$:
+cmp r2,#9
+subhi r2,#10
+addhi r0,#4
+bhi functionLoop$
+
+add r2, r2, lsl #1
+lsl r1, r2
+str r1,[r0]
+pop {pc}
+
